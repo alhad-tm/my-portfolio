38de9b664778654c96b914b8d84341cf7981c053
alhad-tm <alhadchikkutm@gmail.com>
2023-10-20 16:27:30 +0530

    added framer motion

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,29 +1,48 @@
 import React from "react";
 import css from "./About.module.css";
 
+import { motion } from "framer-motion"
+
 const About = () => {
   return (
     <div className={css.container} id="about">
-      <div className={css.heading}>About me</div>
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2}} className={css.heading}>About me</motion.div>
       <div className={css.content}>
-     <span>  I'm a Self-Taught Front-end Web Developer, highly self-motivated and
+     <motion.span 
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1}}
+     >  I'm a Self-Taught Front-end Web Developer, highly self-motivated and
         passionate about learning new things. Moreover, I'm a Tech enthusiast.
-        </span> 
+        </motion.span> 
     
-      <span>  As a base language, I am focusing on JavaScript. On my learning path, I
+        <motion.span 
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1.5}}> As a base language, I am focusing on JavaScript. On my learning path, I
         acquired skills in HTML,CSS, JavaScript, React.js , Next.js, and Git.
-       </span>
-       <span>
+        </motion.span>
+        <motion.span 
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2}}>
         There are so many youtube channels that also helped me to become a
         developer. Along with YouTube; Google, stackoverflow, w3schools, etc.
         are also helpful learning sources to an autodidact person like me.
-        </span>
-        <span>
+        </motion.span>
+        <motion.span 
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2.5}}>
         Following these basic subjects, I made a good understanding of other
         dependent frameworks and libraries like Redux, ReduxToolkit, Bootstrap,
-        and also database knowledge in Firebase. </span>
+        and also database knowledge in Firebase.
+         </motion.span>
       </div>
-    </div>
+    </div> 
   );
 };
 
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,25 +4,41 @@ import css from "./Services.module.css";
 import staticweb from "../../assets/staticweb.png";
 import react from "../../assets/reactjsweb.png";
 import { GoLinkExternal } from "react-icons/go";
+import { motion } from "framer-motion"
+
 
 const Services = () => {
   return (
     <div className={css.container} id="services">
-      <div className={css.heading}>Services</div>
+      <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1}} 
+      className={css.heading}>Services</motion.div>
 
       <div className={css.boxcontainer}>
-        <div className={css.boxes}>
+        <motion.div
+             initial={{ opacity: 0, x: -40 }}
+             whileInView={{ opacity: 1, x: 0 }}
+             transition={{ duration: 1}}
+        className={css.boxes}>
           <span>Static web page</span>
           <img src={staticweb} className={css.imgst} alt="" />
-        </div>
+        </motion.div>
 
-        <div className={css.boxes}>
+        <motion.div className={css.boxes}
+        initial={{ opacity: 0, x: 40 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1}}>
           <span className={css.reactweb}>React web app</span>
           <br />
           <img src={react} className={css.imgr} alt="" />
-        </div>
+        </motion.div>
       </div>
-      <div className={css.servicecontent}>
+      <motion.div className={css.servicecontent}
+      initial={{ opacity: 0, y: 10 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2}}>
         Currently, I am working on Static Web Page development and React Web
         development. Please check my official GitHub page{" "}
         <a href="https://github.com/alhad-tm" target="blank">
@@ -30,7 +46,7 @@ const Services = () => {
         </a>{" "}
         to know more about my works. Given below are some projects that I had
         made from scratch, take a look.
-      </div>
+      </motion.div>
     </div>
   );
 };
