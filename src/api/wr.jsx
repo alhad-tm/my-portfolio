36be17c0ba6af541264aee290a848d060c0afa20
alhad-tm <alhadchikkutm@gmail.com>
2024-11-25 22:56:00 +0530

    rearranged json data

--- src/api/wr.jsx
+++
@@ -1,2 +0,0 @@
-import ima from "../../src/assets/bg4.jpg"
-import uu from "/assets/"
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,97 +1,51 @@
 import React from "react";
 import css from "./Works.module.css";
-
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, EffectCoverflow ,Autoplay} from "swiper";
-import fitclub from "../../assets/fitclubmain.png";
-import foodmain from "../../assets/foodappmains.png";
-import netflix from "../../assets/netflixmain.png";
-import olx from "../../assets/olxmain.jpg";
-import skin from "../../assets/skincaremain.png";
-
+import { Pagination, EffectCoverflow, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
+
 import { GoLinkExternal } from "react-icons/go";
-import workslist from "../../api/workslist.json"
+import workslist from "../../api/workslist.json";
 
 const Works = () => {
   return (
     <div className={css.container} id="works">
-    
-    
-   
-
- 
-  
       <div className={css.heading}>Previous works</div>
-      <Swiper className={css.swiperdiv}
+      <Swiper
+        className={css.swiperdiv}
         effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={3} 
+        slidesPerView={3}
         loop={true}
         speed={2400}
-        autoplay={{ delay: 2000 }} 
-
-        
+        autoplay={{ delay: 2000 }}
         // coverflowEffect={{
         //   rotate: 0,
         //   stretch: 0,
         //   depth: 200,
-        //   modifier: 1, 
+        //   modifier: 1,
         //   slideShadows : false,
         // }}
         pagination={{ clickable: true }}
-        modules={[EffectCoverflow, Pagination,Autoplay]}
-      
+        modules={[EffectCoverflow, Pagination, Autoplay]}
       >
-
-  
-  
-  
-    <div className={css.box}>
-   
-  {workslist.map((myworks,index)=>( 
-     <SwiperSlide className={css.sslide}>
-          <img src={myworks.image} className={css.projectimages}/>
-          {myworks.link?   <a href={myworks?.link} target="blank">
-              <GoLinkExternal className={css.externallink} />
-            </a>
-          :""  }
-         
-          </SwiperSlide>
-  ))} 
-  
-          {/* <SwiperSlide className={css.sslide}>
-            <img src={fitclub} alt="" className={css.projectimages} />{" "}
-            <a href="https://fitclub-app-37f.pages.dev/" target="blank">
-              <GoLinkExternal className={css.externallink} />
-            </a>
-          </SwiperSlide>
-          <SwiperSlide className={css.sslide}>
-            {" "}
-            <img src={foodmain} alt="" className={css.projectimages} />
-          </SwiperSlide>
-          <SwiperSlide className={css.sslide}>
-            {" "}
-            <img src={netflix} alt="" className={css.projectimages} />
-            <a href="https://netflixclone-al.netlify.app/" target="blank" >
-              <GoLinkExternal className={css.externallink} /> 
-            </a>
-          </SwiperSlide>
-          <SwiperSlide className={css.sslide}>
-            {" "}
-            <img src={olx} alt="" className={css.projectimages} />
-          </SwiperSlide>
-          <SwiperSlide className={css.sslide}>
-            {" "}
-            <img src={skin} alt="" className={css.projectimages} />
-            <a href="https://skincare01.netlify.app/" target="blank">
-              <GoLinkExternal className={css.externallink} />
-            </a>
-          </SwiperSlide> */}
+        <div className={css.box}>
+          {workslist.map((myworks, index) => (
+            <SwiperSlide className={css.sslide}>
+              <img src={myworks.image} className={css.projectimages} />
+              {myworks.link ? (
+                <a href={myworks?.link} target="blank">
+                  <GoLinkExternal className={css.externallink} />
+                </a>
+              ) : (
+                ""
+              )}
+            </SwiperSlide>
+          ))}
         </div>
         <br />
       </Swiper>
